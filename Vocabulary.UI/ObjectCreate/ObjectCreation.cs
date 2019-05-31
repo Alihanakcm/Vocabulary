3fbbb7af7a86ad754bbcbd3d7ae91fd7266bd07f
Alihan Akçam <42321382+Alihanakcm@users.noreply.github.com>
2019-05-31 06:32:38 +0300

    ObjectCreation

---
+++ Vocabulary.UI/ObjectCreate/ObjectCreation.cs
@@ -0,0 +1,64 @@
+﻿namespace Vocabulary.MainUI
+{
+    public class ObjectCreation
+    {
+        private ObjectCreation()
+        {
+            
+        }
+        private static ExamForm _ExamForm;
+
+        public static void examForm()
+        {
+            if (_ExamForm==null || _ExamForm.IsDisposed)
+            {
+                _ExamForm=new ExamForm();
+                _ExamForm.Show();
+            }           
+        }
+
+        private static SettingsForm _SettingsForm;
+
+        public static void settingsForm()
+        {
+            if (_SettingsForm==null || _SettingsForm.IsDisposed)
+            {
+                _SettingsForm=new SettingsForm();
+                _SettingsForm.Show();
+            }
+        }
+
+        private static WordLearnForm _WordLearnForm;
+
+        public static void wordLearnForm()
+        {
+            if (_WordLearnForm==null || _WordLearnForm.IsDisposed)
+            {
+                _WordLearnForm=new WordLearnForm();
+                _WordLearnForm.Show();
+            }
+        }
+
+        private static WordOperationsForm _WordOperationsForm;
+
+        public static void wordOperationsForm()
+        {
+            if (_WordOperationsForm==null || _WordOperationsForm.IsDisposed)
+            {
+                _WordOperationsForm=new WordOperationsForm();
+                _WordOperationsForm.Show();
+            }
+        }
+
+        private static StatisticsForm _StatisticsForm;
+
+        public static void statisticsForm()
+        {
+            if (_StatisticsForm==null || _StatisticsForm.IsDisposed)
+            {
+                _StatisticsForm=new StatisticsForm();
+                _StatisticsForm.Show();
+            }
+        }
+    }
+}
